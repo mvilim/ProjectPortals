e82e204d03af4a890fe446b11698fd046d206868
TrenTech <TrenTech@gmail.com>
2018-01-03 20:04:27 -0500

    Update deprecated stuff - untested

--- src/main/java/com/gmail/trentech/pjp/data/Keys.java
+++ src/main/java/com/gmail/trentech/pjp/data/Keys.java
@@ -1,10 +1,7 @@
 package com.gmail.trentech.pjp.data;
 
-import java.util.Map;
-
 import org.spongepowered.api.data.DataQuery;
 import org.spongepowered.api.data.key.Key;
-import org.spongepowered.api.data.key.KeyFactory;
 import org.spongepowered.api.data.value.mutable.MapValue;
 import org.spongepowered.api.data.value.mutable.Value;
 
@@ -13,19 +10,14 @@ import com.google.common.reflect.TypeToken;
 
 public class Keys {
 
-	private static final TypeToken<Map<String, Portal>> PORTALS_MAP_TOKEN = new TypeToken<Map<String, Portal>>() {
-		private static final long serialVersionUID = -1;
-	};
 	private static final TypeToken<MapValue<String, Portal>> PORTALS_MAP_VALUE_TOKEN = new TypeToken<MapValue<String, Portal>>() {
 		private static final long serialVersionUID = -1;
 	};
 	private static final TypeToken<Value<Portal>> PORTAL_VALUE_TOKEN = new TypeToken<Value<Portal>>() {
 		private static final long serialVersionUID = -1;
 	};
-	private static final TypeToken<Portal> PORTAL_TOKEN = new TypeToken<Portal>() {
-		private static final long serialVersionUID = -1;
-	};
 
-	public static final Key<Value<Portal>> PORTAL = KeyFactory.makeSingleKey(PORTAL_TOKEN, PORTAL_VALUE_TOKEN, DataQuery.of("portal"), "pjp:portal", "portal");
-	public static final Key<MapValue<String, Portal>> PORTALS = KeyFactory.makeMapKey(PORTALS_MAP_TOKEN, PORTALS_MAP_VALUE_TOKEN, DataQuery.of("portals"), "pjp:portals", "portals");
+	public static final Key<Value<Portal>> PORTAL = Key.builder().type(PORTAL_VALUE_TOKEN).id("pjp:portal").name("portal").query(DataQuery.of("portal")).build();
+	public static final Key<MapValue<String, Portal>> PORTALS = Key.builder().type(PORTALS_MAP_VALUE_TOKEN).id("pjp:portals").name("portals").query(DataQuery.of("portals")).build();
+
 }
